c6d74031f1f00980c7ec54c8f94c863d65935c47
Szymon Cader <szymon.SC.cader@gmail.com>
2019-12-30 12:23:08 +0100

    heroku.yml

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $DEPLOYMENT == "true" ]]
+then
+  python manage.py migrate
+fi
+
+if [[ $DJANGO_SETTINGS_MODULE == "teleagh.settings.production" ]]
+then
+  echo "Starting gunicorn"
+  gunicorn --bind 0.0.0.0:$PORT teleagh.wsgi
+else
+  echo "Starting developer server"
+  python manage.py runserver 0.0.0.0:$PORT
+fi
