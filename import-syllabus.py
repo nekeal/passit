2984e12e0114e705a7bd54cda9b0e7339d1614f3
Szymon Cader <szymon.SC.cader@gmail.com>
2019-12-31 14:37:07 +0100

    test

--- import-syllabus.py
+++ import-syllabus.py
@@ -2,10 +2,9 @@ import django
 import json
 import random
 import requests
-from typing import Dict, Any
-
-random.choices()
+from typing import Dict, Any, List, Union
 django.setup()
+JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
 fields_of_studies = ['stacjonarne-informatyka',
                      'stacjonarne-cyberbezpieczenstwo--2',
                      'stacjonarne-elektronika',
@@ -17,9 +16,9 @@ list_of_subjects_url = 'https://syllabuskrk.agh.edu.pl/2019-2020/magnesite/api/f
 detail_of_subject_url = 'https://syllabuskrk.agh.edu.pl/api/current_annual/modules/{}'
 list_of_subjects = []
 for field in fields_of_studies:
-    result = requests.get(list_of_subjects_url.format(field))
-    result = result.json()['syllabus']['assignments']
-    for syllabus_subject in result:
+    result: Dict[str, Any] = requests.get(list_of_subjects_url.format(field)).json()
+    subject_list: List[Any] = result['syllabus']['assignments']
+    for syllabus_subject in subject_list:
         subject = {}
         subject['id'] = syllabus_subject['assignment']['module_id']
         subject['code'] = syllabus_subject['assignment']['module_code']
