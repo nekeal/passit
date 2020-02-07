7c610efa356d36e5e100d12012e5ee718a600a63
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-07 13:02:12 +0100

    Rewrite serializers in subject app to use drf-flex-fields

--- subject/serializers.py
+++ subject/serializers.py
@@ -1,58 +1,35 @@
+from typing import Dict, Any, Tuple
+
+from rest_flex_fields import FlexFieldsModelSerializer
 from rest_framework import serializers
-from rest_framework_serializer_extensions.serializers import SerializerExtensionsMixin
+from rest_framework.serializers import Serializer
 
 from subject.models import FieldOfStudies, Subject, Resource
 
 
-class FieldOfStudiesSerializer(serializers.ModelSerializer):
+class FieldOfStudiesBaseSerializer(FlexFieldsModelSerializer):
 
     class Meta:
         model = FieldOfStudies
         fields = ('id', 'name', 'slug')
 
 
-class SubjectListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Subject
-        fields = ('id', 'name', 'semester',)
-
-
-class SubjectDetailSerializer(serializers.ModelSerializer):
+class SubjectBaseSerializer(FlexFieldsModelSerializer):
     field_of_studies = serializers.CharField(source='field_of_studies.name')
 
     class Meta:
         model = Subject
         fields = ('id', 'name', 'semester', 'general_description', 'field_of_studies')
+        expandable_fields: Dict[str, Tuple[Serializer, Dict[str, Any]]] = {
+            'field_of_studies': (FieldOfStudiesBaseSerializer, {})
+        }
 
 
-class ResourceListSerializer(serializers.ModelSerializer):
-    is_image = serializers.SerializerMethodField(source='image', read_only=True)
-    is_url = serializers.SerializerMethodField(source='url', read_only=True)
-
-    def get_is_image(self, instance):
-        return True if instance.image else False
-
-    def get_is_url(self, instance):
-        return True if instance.url else False
+class ResourceBaseSerializer(FlexFieldsModelSerializer):
 
     class Meta:
         model = Resource
-        fields = ('id', 'name', 'is_url', 'is_image', 'url')
-
-
-class ResourceDetailSerializer(SerializerExtensionsMixin, serializers.ModelSerializer):
-
-    class Meta:
-        model = Resource
-        fields = ('id', 'name', 'image', 'url', 'description', 'created_by', 'modified_by')
-        expandable_fields = dict(
-            subject=dict(
-                serializer=SubjectListSerializer,
-                read_only=False
-            )
-        )
-    #     
-    # def create(self, validated_data):
-    #     validated_data['subject'] = validated_data.pop('subject_id_resolved')
-    #     return super(ResourceDetailSerializer, self).create(validated_data)
+        fields = ('id', 'name', 'image', 'url', 'description', 'subject', 'created_by', 'modified_by')
+        expandable_fields: Dict[str, Tuple[Serializer, Dict[str, Any]]] = {
+            'subject': (SubjectBaseSerializer, {'fields': ['id', 'name', 'semester', ]})
+        }
--- subject/views.py
+++ subject/views.py
@@ -1,34 +1,21 @@
+from rest_flex_fields import FlexFieldsModelViewSet
 from rest_framework import viewsets
-from rest_framework_serializer_extensions.views import SerializerExtensionsAPIViewMixin
 
 from subject.models import FieldOfStudies, Subject, Resource
-from subject.serializers import FieldOfStudiesSerializer, SubjectListSerializer, SubjectDetailSerializer, \
-    ResourceListSerializer, ResourceDetailSerializer
+from subject.serializers import FieldOfStudiesBaseSerializer, SubjectBaseSerializer, ResourceBaseSerializer
 
 
-class FieldOfStudiesViewSet(viewsets.ModelViewSet):
-    serializer_class = FieldOfStudiesSerializer
+class FieldOfStudiesViewSet(FlexFieldsModelViewSet):
+    serializer_class = FieldOfStudiesBaseSerializer
     queryset = FieldOfStudies.objects.all()
-    model = FieldOfStudies
 
 
 class SubjectViewSet(viewsets.ModelViewSet):
-    serializer_class = SubjectListSerializer
+    serializer_class = SubjectBaseSerializer
     queryset = Subject.objects.all()
 
-    def get_serializer_class(self):
-        if self.action != 'list':
-            return SubjectDetailSerializer
-        return super(SubjectViewSet, self).get_serializer_class()
 
-
-class ResourceViewSet(SerializerExtensionsAPIViewMixin, viewsets.ModelViewSet):
-    serializer_class = ResourceListSerializer
+class ResourceViewSet(FlexFieldsModelViewSet):
+    serializer_class = ResourceBaseSerializer
     queryset = Resource.objects.all()
-    # extensions_expand = {"subject"}
-    # extensions_only = {'name', 'url'}
-    def get_serializer_class(self):
-        if self.action != 'list':
-            print("DETAIL")
-            return ResourceDetailSerializer
-        return super(ResourceViewSet, self).get_serializer_class()
+    permit_list_expands = ('subject',)
