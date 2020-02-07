46a3d3661840ec92387c0a19440ba2a304276ac3
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-07 18:08:06 +0100

    refactor and add factory_boy to requirements

--- subject/factories.py
+++ subject/factories.py
@@ -6,46 +6,42 @@ from subject.models import FieldOfStudies, Subject, SubjectOfAgeGroup, Resource,
 
 
 class FieldOfStudiesFactory(factory.DjangoModelFactory):
+    name = factory.sequence(lambda n: f'field{n}')
 
     class Meta:
         model = FieldOfStudies
 
-    name = factory.sequence(lambda n: f'field{n}')
-
 
 class SubjectFactory(factory.DjangoModelFactory):
-
-    class Meta:
-        model = Subject
 
     name = factory.sequence(lambda n: f'subject{n}')
     semester = factory.LazyAttribute(lambda n: randint(1, 6))
     general_description = "description"
     field_of_studies = factory.SubFactory(FieldOfStudiesFactory)
 
+    class Meta:
+        model = Subject
+
 
 class SubjectOfAgeGroupFactory(factory.DjangoModelFactory):
+    subject = factory.SubFactory(SubjectFactory)
+    students_start_year = factory.LazyAttribute(lambda n: datetime.datetime.now().year - 1)
 
     class Meta:
         model = SubjectOfAgeGroup
 
-    subject = factory.SubFactory(SubjectFactory)
-    students_start_year = factory.LazyAttribute(lambda n: datetime.datetime.now().year - 1)
-
 
 class ExamFactory(factory.DjangoModelFactory):
+    subject_group = factory.SubFactory(SubjectOfAgeGroupFactory)
+    starts_at = datetime.datetime.now() + datetime.timedelta(days=30)
 
     class Meta:
         model = Exam
 
-    subject_group = factory.SubFactory(SubjectOfAgeGroupFactory)
-    starts_at = datetime.datetime.now() + datetime.timedelta(days=30)
-
 
 class ResourceFactory(factory.DjangoModelFactory):
+    name = factory.sequence(lambda n: f"Resource {n}")
+    subject = factory.SubFactory(Subject)
 
     class Meta:
         model = Resource
-
-    name = factory.sequence(lambda n: f"Resource {n}")
-    subject = factory.SubFactory(Subject)
