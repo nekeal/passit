015b5ca72e42045d32089bf44cf9655d79086d3f
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-11 18:08:44 +0100

    WIKI-9 Add field of study of age group

--- accounts/models.py
+++ accounts/models.py
@@ -10,5 +10,11 @@ class UserProfile(models.Model):
     user = models.OneToOneField('CustomUser', on_delete=models.CASCADE, related_name='profile')
     own_field_group = models.ForeignKey('subject.FieldOfStudyOfAgeGroup', on_delete=models.PROTECT,
                                         null=True, blank=True, related_name='representatives')
-    field_groups = models.ManyToManyField('subject.FieldOfStudyOfAgeGroup', blank=True,
+    field_groups = models.ManyToManyField('subject.FieldOfStudyOfAgeGroup', blank=True, through='Membership',
                                           related_name='students')
+
+
+class Membership(models.Model):
+    profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name='memberships')
+    field_group = models.ForeignKey('subject.FieldOfStudyOfAgeGroup', on_delete=models.CASCADE,
+                                    related_name='memberships')
