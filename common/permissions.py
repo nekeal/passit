de3430c6ae63f7c5c6800edb6864b6d836b1aee2
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-26 10:08:57 +0100

    Fix permission

--- common/permissions.py
+++ common/permissions.py
@@ -1,11 +1,13 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsRepresentativeOrModeratorOrReadOnly(BasePermission):
+class IsPrivilegedOrReadOnly(BasePermission):
 
     def has_permission(self, request, view) -> bool:
         return bool(
-            request.method in SAFE_METHODS
-            or request.user
-            and request.user.profile.is_representative_or_moderator()
-        )
+            request.user.is_authenticated
+            and (
+                request.method in SAFE_METHODS
+                or request.user
+                and request.user.profile.is_privileged()
+            ))
