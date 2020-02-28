cd11111f25d755b501906dc9ed090a0f16387c53
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-28 22:28:21 +0100

    Add custom manager and querysets for subject app

--- teleagh/subject/models.py
+++ teleagh/subject/models.py
@@ -3,6 +3,10 @@ import datetime
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from .managers import SubjectManager, SubjectOfAgeGroupManager, FieldOfStudyManager,\
+    FieldOfStudyOfAgeGroupManager, ResourceManager
+from .querysets import SubjectQuerySet, SubjectOfAgeGroupQuerySet, FieldOfStudyQuerySet,\
+    FieldOfStudyOfAgeGroupQuerySet, ResourceQuerySet
 from ..common.models import TimeStampedModel, OwnedModel
 from ..lecturers.models import LecturerOfSubject, Lecturer
 
@@ -18,6 +22,8 @@ class FieldOfStudy(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, help_text='Slug which identifies field of study in syllabus')
 
+    objects = FieldOfStudyManager.from_queryset(FieldOfStudyQuerySet)()
+
     def __str__(self) -> str:
         return self.name
 
@@ -26,8 +32,22 @@ class FieldOfStudyOfAgeGroup(models.Model):
     field_of_study = models.ForeignKey('FieldOfStudy', on_delete=models.PROTECT, related_name='age_groups')
     students_start_year = models.PositiveIntegerField(validators=[year_validator, ])
 
+    objects = FieldOfStudyOfAgeGroupManager.from_queryset(FieldOfStudyOfAgeGroupQuerySet)()
+
     def __str__(self):
         return f'{self.field_of_study} {self.students_start_year}'
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+    semester = models.IntegerField()
+    general_description = models.TextField()
+    field_of_study = models.ForeignKey('FieldOfStudy', on_delete=models.PROTECT, related_name='subjects')
+
+    objects = SubjectManager.from_queryset(SubjectQuerySet)()
+
+    def __str__(self) -> str:
+        return f'{self.name}'
 
 
 class Resource(TimeStampedModel, OwnedModel):
@@ -38,18 +58,10 @@ class Resource(TimeStampedModel, OwnedModel):
 
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE, related_name='resources')
 
+    objects = ResourceManager.from_queryset(ResourceQuerySet)()
+
     def __str__(self) -> str:
         return f'{self.name} - {self.subject}'
-
-
-class Subject(models.Model):
-    name = models.CharField(max_length=50)
-    semester = models.IntegerField()
-    general_description = models.TextField()
-    field_of_study = models.ForeignKey('FieldOfStudy', on_delete=models.PROTECT, related_name='subjects')
-
-    def __str__(self) -> str:
-        return f'{self.name}'
 
 
 class SubjectOfAgeGroup(models.Model):
@@ -58,6 +70,8 @@ class SubjectOfAgeGroup(models.Model):
                                         related_name='subject_groups')
     description = models.TextField(blank=True)
     lecturers = models.ManyToManyField(Lecturer, through=LecturerOfSubject, related_name='lecturer_age_groups')
+
+    objects = SubjectOfAgeGroupManager.from_queryset(SubjectOfAgeGroupQuerySet)()
 
     def __str__(self) -> str:
         return f'{self.subject} {self.field_age_group}'
