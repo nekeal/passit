d661207a90e66caae32ed59e465244b2fd1337f5
Szymon Cader <szymon.SC.cader@gmail.com>
2020-02-29 21:59:46 +0100

    Change owned model to point to profile instead of user

--- teleagh/common/models.py
+++ teleagh/common/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from teleagh.accounts.models import UserProfile
+
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,10 +12,13 @@ class TimeStampedModel(models.Model):
 
 
 class OwnedModel(models.Model):
-    created_by = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE,
+    created_by = models.ForeignKey('accounts.UserProfile', on_delete=models.CASCADE,
                                    blank=True, null=True, related_name='%(class)s_created')
-    modified_by = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE,
+    modified_by = models.ForeignKey('accounts.UserProfile', on_delete=models.CASCADE,
                                     blank=True, null=True, related_name='%(class)s_modified')
 
     class Meta:
         abstract = True
+
+    def is_owner(self, profile: UserProfile) -> bool:
+        return profile == self.created_by
