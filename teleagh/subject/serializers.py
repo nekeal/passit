6e3049d9595f7aa3d083370bbf3e6cafe14b6ef0
Szymon Cader <szymon.SC.cader@gmail.com>
2020-03-11 16:53:50 +0100

    WIKI-15 Add lecturers to subject serializer

--- teleagh/subject/serializers.py
+++ teleagh/subject/serializers.py
@@ -4,6 +4,8 @@ from rest_flex_fields import FlexFieldsModelSerializer
 from rest_framework import serializers
 from rest_framework.serializers import Serializer
 
+from ..lecturers.models import LecturerOfSubjectOfAgeGroup
+from ..lecturers.serializers import LecturerOfSubjectOfAgeGroupSerializer
 from ..subject.models import FieldOfStudy, Subject, Resource, FieldOfStudyOfAgeGroup
 
 
@@ -27,11 +29,18 @@ class FieldOfStudyOfAgeGroupSerializer(FlexFieldsModelSerializer):
 class SubjectBaseSerializer(FlexFieldsModelSerializer):
     field_of_study = serializers.CharField(source='field_of_study.name')
 
+    def get_lecturers(self, instance: Subject):
+        lecturers_of_age_group = LecturerOfSubjectOfAgeGroup.objects.filter(subject_group__subject=instance.id).\
+            annotate_students_start_year()
+        serializer = LecturerOfSubjectOfAgeGroupSerializer(lecturers_of_age_group, many=True, expand=['lecturer', 'students_start_year'])
+        return serializer.data
+
     class Meta:
         model = Subject
         fields = ('id', 'name', 'semester', 'general_description', 'field_of_study')
         expandable_fields: Dict[str, Tuple[Serializer, Dict[str, Any]]] = {
-            'field_of_study': (FieldOfStudyBaseSerializer, {})
+            'field_of_study': (FieldOfStudyBaseSerializer, {}),
+            'lecturers': (serializers.SerializerMethodField, {})
         }
 
 
