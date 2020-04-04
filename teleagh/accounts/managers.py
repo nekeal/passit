b46d1f1e22ef33c6bc7ed753ffb4891208fb5a1e
Szymon Cader <szymon.SC.cader@gmail.com>
2020-04-04 16:20:57 +0200

    Create method on user managers

--- teleagh/accounts/managers.py
+++ teleagh/accounts/managers.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 from django.contrib.auth.models import UserManager
 from django.db.models import Manager, QuerySet
@@ -6,12 +6,20 @@ from django.db.models import Manager, QuerySet
 from .querysets import UserProfileQuerySet, CustomUserQuerySet, MembershipQuerySet
 
 if TYPE_CHECKING:
-    from .models import UserProfile, Membership
+    from .models import UserProfile, Membership, FieldOfStudyOfAgeGroup
 
 
 class CustomUserManager(UserManager.from_queryset(CustomUserQuerySet)):  # type: ignore
     def get_queryset(self):
         return super().get_queryset().select_related('profile')
+
+    def create_student(self, username: str, field_age_group: 'FieldOfStudyOfAgeGroup', type: int,
+                       email: Optional[str] = None, password: Optional[str] = None):
+        from .models import UserProfile, Membership
+        user = super(CustomUserManager, self).create_user(username, email, password)
+        profile = UserProfile.objects.create(user=user)
+        Membership.objects.create(profile=profile, field_age_group=field_age_group)
+        return user
 
 
 class UserProfileManager(Manager):  # type: ignore
