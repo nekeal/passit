cd3959070765330dc6d5cccdd0bfaeca1dd7f966
Szymon Cader <szymon.SC.cader@gmail.com>
2020-04-07 13:47:38 +0200

    Setup redis

--- teleagh/settings/base.py
+++ teleagh/settings/base.py
@@ -95,6 +95,16 @@ DATABASES = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('REDIS_LOCATION', "redis://127.0.0.1:6379/1"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "passit"
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
