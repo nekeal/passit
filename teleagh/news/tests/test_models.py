8b4e2b7df1f87b2ec440108065d0d2cb58daffb8
Szymon Cader <szymon.SC.cader@gmail.com>
2020-04-09 13:20:21 +0200

    Fix news model tests

--- teleagh/news/tests/test_models.py
+++ teleagh/news/tests/test_models.py
@@ -1,24 +1,24 @@
 from ..factories import NewsFactory
 from ..models import News
 from ...accounts.factories import MembershipFactory
-from ...subject.factories import SubjectOfAgeGroupFactory
+from ...subject.factories import SubjectOfAgeGroupFactory, FieldOfStudyOfAgeGroupFactory
 
 
-def test_get_news_by_profile(user_profile1_with_membership, subject_group):
-    expected_news = NewsFactory.create_batch(3, subject_group=subject_group)
-    NewsFactory.create_batch(2, subject_group=SubjectOfAgeGroupFactory())
-    user_news = News.objects.get_by_profile(user_profile1_with_membership)
+def test_get_news_by_profile(student1, field_age_group):
+    expected_news = NewsFactory.create_batch(3, field_age_group=field_age_group)
+    NewsFactory.create_batch(2, field_age_group=FieldOfStudyOfAgeGroupFactory())
+    user_news = News.objects.get_by_profile(student1)
     assert len(user_news) == 3, "method gets wrong news for user"
     assert set(user_news) == set(expected_news), "method gets wrong news for user"
 
 
-def test_get_news_by_profile_with_multiple_memberships(user_profile1_with_membership, subject_group):
+def test_get_news_by_profile_with_multiple_memberships(student1, subject_group):
     another_subject_of_age_group = SubjectOfAgeGroupFactory()
-    MembershipFactory(profile=user_profile1_with_membership,
+    MembershipFactory(profile=student1,
                       field_age_group=another_subject_of_age_group.field_age_group)
     expected_news = NewsFactory.create_batch(1, subject_group=subject_group)
     expected_news += NewsFactory.create_batch(1, subject_group=another_subject_of_age_group)
     NewsFactory.create_batch(2, subject_group=SubjectOfAgeGroupFactory())
-    user_news = News.objects.get_by_profile(user_profile1_with_membership)
+    user_news = News.objects.get_by_profile(student1)
     assert len(user_news) == 2, "method gets wrong number of news for user"
     assert list(user_news) == expected_news, "method gets wrong news for user"
