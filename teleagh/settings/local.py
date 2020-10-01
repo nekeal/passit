d3318817c9aecb750dadb920ea0b3509ece3c1bb
Szymon Cader <szymon.sc.cader@gmail.com>
2020-10-01 23:36:10 +0200

    PASSIT-10003 fix db settings

--- teleagh/settings/local.py
+++ teleagh/settings/local.py
@@ -11,7 +11,7 @@ SILKY_PYTHON_PROFILER = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'teleagh2'),
+        'NAME': os.environ.get('POSTGRES_DB', 'teleagh'),
         'USER': os.environ.get('POSTGRES_USER', 'teleagh'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'teleagh'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
